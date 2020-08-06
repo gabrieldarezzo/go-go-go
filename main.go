45634115fca6bbeba7e3a5b4d0dcfc93b0b66f85
Guilherme Henrique Rodrigues <guilherme.rodrigues@picpay.com>
2020-08-06 17:57:03 -0300

    Get number by io

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
-	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
-
-
 
 func main() {
 
-	
+	number1, _ := RetrieveNumberByIo()
+	number2, _ := RetrieveNumberByIo()
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ") 
-	text, _ := reader.ReadString('\n')
-
-	fmt.Println("Vc digitou:") 
-	fmt.Println(text)
-
-	sumValue, err := Sum(1, 2)
+	sumValue, _ := Sum(number1, number2)
 
 	//subValue, err := Subtraction(5, 3)
 
@@ -28,18 +22,17 @@ func main() {
 
 	//divideValue, err := Divide(2, 0)
 
-	
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
 	fmt.Println(sumValue)
 	//fmt.Println(divideValue)
-	
+
 }
 
-func Sum(num1, num2 int) (int, error) {
+func Sum(num1, num2 float64) (float64, error) {
 	return num1 + num2, nil
 }
 
@@ -56,4 +49,13 @@ func Divide(num1, num2 float64) (float64, error) {
 		return 0, errors.New("Wrong value")
 	}
 	return num1 / num2, nil
+}
+
+func RetrieveNumberByIo() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number: ")
+	text, _ := reader.ReadString('\n')
+	result := strings.Split(strings.TrimSpace(text), "x")
+
+	return strconv.ParseFloat(result[0], 64)
 }
