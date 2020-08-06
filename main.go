bbc18fa31aa3be39ebcf55100bfe220e5c74e77d
Djeane Barros Carvalho <djeane.carvalho@PPM-SPO-8135.local>
2020-08-06 17:09:28 -0300

    added subtraction

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 )
 
 func main() {
-	sumValue, err := Sum(1, 2)
+	//sumValue, err := Sum(1, 2)
+
+	subValue, err := Subtraction(5, 3)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(sumValue)
+	fmt.Println(subValue)
 }
 
 func Sum(num1, num2 int) (int, error) {
 	return num1 + num2, nil
+}
+
+func Subtraction(num1, num2 float64) (float64, error) {
+	return num1 - num2, nil
 }
