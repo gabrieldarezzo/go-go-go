8bcb7777b47996bc0ca2e243ef41c9a5290324b0
Lucas Onofre <lucas-onofre@hotmail.com>
2020-08-06 17:19:16 -0300

    Added miltiplication and Divide functions

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 	//sumValue, err := Sum(1, 2)
 
-	subValue, err := Subtraction(5, 3)
+	//subValue, err := Subtraction(5, 3)
+
+	multiValue, err := Multi(5, 3)
+
+	divideValue, err := Divide(2, 0)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(subValue)
+	fmt.Println(multiValue)
+	fmt.Println(divideValue)
 }
 
 func Sum(num1, num2 int) (int, error) {
@@ -23,4 +29,15 @@ func Sum(num1, num2 int) (int, error) {
 
 func Subtraction(num1, num2 float64) (float64, error) {
 	return num1 - num2, nil
+}
+
+func Multi(num1, num2 float64) (float64, error) {
+	return num1 * num2, nil
+}
+
+func Divide(num1, num2 float64) (float64, error) {
+	if num2 == 0 {
+		return 0, errors.New("Wrong value")
+	}
+	return num1 / num2, nil
 }
